052fbf0f4d3b229159412faf86c0868b91e81a50
asimon <Alexander.Simon@davero.de>
2022-01-18 23:46:29 +0100

    added some scripts

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,16 @@ bootstrapExtra().catch(e => console.error(e));
 
 let currentPopup: any = undefined;
 const today = new Date();
-const time = today.getHours() + ":" + today.getMinutes();
+const friday = new Date();
+friday.setDate(friday.getDate() + (6 + (7 - friday.getDay())) % 7);
+  
+const difference_In_Time = friday.getTime() - today.getTime();
+const difference_In_Days = difference_In_Time / (1000 * 3600 * 24);
+const time = difference_In_Days;
 
 WA.room.onEnterLayer('clockZone').subscribe(() => {
     console.log('toto')
-    currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
+    currentPopup =  WA.ui.openPopup("clockPopup",`Noch ${time} ${time > 1 ? 'Tage' : 'Tag' } bis zum Wochenende 🥳`,[]);
 })
 
 WA.room.onLeaveLayer('clockZone').subscribe(closePopUp)
@@ -22,3 +27,12 @@ function closePopUp(){
         currentPopup = undefined;
     }
 }
+
+
+WA.onInit().then(() => {
+    // console.log('Tags: ', WA.player);
+    WA.chat.sendChatMessage(`Herzlich Willkommen ${WA.player.name}, in unserem digitalen Office!`, 'Alex');
+})
+
+
+
