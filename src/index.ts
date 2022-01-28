ec9cede34de010d9508e1661ffadf53f9bce90d8
asimon <Alexander.Simon@davero.de>
2022-01-28 18:14:17 +0100

    added forest and garden sound

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
+/// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
 import {bootstrapExtra} from "@workadventure/scripting-api-extra";
 
 // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure.
